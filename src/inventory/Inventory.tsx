7f545bdd513f5d4de8bdddda28aca7192895873f
gavand-sagar <gavand.sagar.s@gmail.com>
2024-02-07 17:17:33 +0530

    widget

--- src/inventory/Inventory.tsx
+++ src/inventory/Inventory.tsx
@@ -1,8 +1,31 @@
+import { render, screen } from "@testing-library/react"
+import BarChart from "../core/compoents/BarChart"
+import Widget from "../core/compoents/Widget"
 
 type Props = {}
 
 export default function Inventory({ }: Props) {
+    
     return (
-        <div>Inventory</div>
+        <div>
+            <Widget heading="Bar Chart">
+                
+            </Widget>
+            
+            <hr/>
+            <Widget heading="Bar Chart">
+                
+            </Widget>
+        </div>
     )
 }
+
+
+// test('', () => {
+//     render(<BarChart dropdownData={["", "", "", "", ""]}></BarChart>)
+//     expect(screen.getAllByRole('option').length).toBe(5)
+// },)
+// test('', () => {
+//     render(<BarChart dropdownData={["", "", "", "", ""]}></BarChart>)
+//     expect(screen.getAllByRole('option').length).toBe(5)
+// },)
